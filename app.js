9c725f23e9b41f1fcd82a1f884645ba3524dd20a
Richard Le <169632088+rlechuong@users.noreply.github.com>
2026-05-31 18:21:49 -0400

    chore: set up EJS view engine and index router

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from "express";
+import path from "node:path";
+import { indexRouter } from "./routes/indexRouter.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Test");
-});
+app.set("views", path.join(import.meta.dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
 
 app.listen(PORT, (error) => {
   if (error) {
