cc916a8b57a1b3cfb869af65a5482cbbe89265b6
Richard Le <169632088+rlechuong@users.noreply.github.com>
2026-05-31 19:25:31 -0400

    feat: add index view with message list

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,8 +2,21 @@ import { Router } from "express";
 
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 indexRouter.get("/", (req, res) => {
-  res.send("GET Index Page");
+  res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
 export { indexRouter };
