3b29c6b35adaf99f326acab97d30e1a95fc0c91a
Richard Le <169632088+rlechuong@users.noreply.github.com>
2026-05-31 22:44:58 -0400

    feat: add new message form and POST route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -18,5 +18,13 @@ const messages = [
 indexRouter.get("/", (req, res) => {
   res.render("index", { title: "Mini Message Board", messages: messages });
 });
+indexRouter.get("/new", (req, res) => {
+  res.render("form");
+});
+indexRouter.post("/new", (req, res) => {
+  const { messageText, messageUser } = req.body;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
+});
 
 export { indexRouter };
